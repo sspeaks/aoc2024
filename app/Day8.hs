8ed7b2f7c2146aca9fdf90a3106b83ea46be2937
Seth Speaks <sspeaks610@gmail.com>
2024-12-12 14:57:28 -0800

    copy allen malloys solutions to Day8 and Day9

--- app/Day8.hs
+++ app/Day8.hs
@@ -1,25 +1,54 @@
 module Day8 where
 
 import Control.Arrow ((&&&))
-import Text.Parsec (Parsec, runParser)
+import Control.Monad (join)
+import Data.Char (isAlphaNum)
+import Data.Containers.ListUtils (nubOrd)
+import Data.Foldable (toList)
+import Data.Ix (inRange)
+import Data.Map.Strict qualified as M
+import Data.Set qualified as S
+import Linear.V2 (V2 (..))
 
-type Parser = Parsec String ()
+type Coord = V2 Int
 
-type Input = ()
+type Frequency = Char
 
-part1 :: Input -> ()
-part1 _ = ()
+data Input = Input (Coord, Coord) (M.Map Frequency (S.Set Coord)) deriving (Show)
 
-part2 :: Input -> ()
-part2 _ = ()
+solve :: (Coord -> Coord -> [Coord]) -> Input -> Int
+solve extend (Input bounds freqs) = length . nubOrd $ antinodes
+  where
+    antinodes = do
+      (_, antennas) <- M.assocs freqs
+      (a, b) <- filter (uncurry (/=)) . toList . join S.cartesianProduct $ antennas
+      takeWhile (inRange bounds) (extend a b)
+
+part1 :: Input -> Int
+part1 = solve $ \a b -> [a + (a - b)]
+
+part2 :: Input -> Int
+part2 = solve $ \a b -> iterate (+ (a - b)) a
+
+labelGrid :: String -> ((V2 Int, V2 Int), [(V2 Int, Char)])
+labelGrid text =
+  ( (V2 1 1, V2 (length rows) (length $ head rows)),
+    concat $ zipWith (\y r -> zipWith (\x c -> (V2 y x, c)) [1 ..] r) [1 ..] rows
+  )
+  where
+    rows = lines text
 
 prepare :: String -> Input
-prepare inp = case runParser parser () "" inp of
-  (Left e) -> error $ show e
-  (Right res) -> res
+prepare text = Input bounds antennas
   where
-    parser :: Parser Input
-    parser = return ()
+    (bounds, indexed) = labelGrid text
+    antennas = M.fromListWith S.union $ do
+      (pos, c) <- indexed
+      case c of
+        '.' -> []
+        x
+          | isAlphaNum x -> [(x, S.singleton pos)]
+          | otherwise -> error $ "Unexpected character " <> [x]
 
 main :: IO ()
 main = readFile "inputs/day8input.txt" >>= print . (part1 &&& part2) . prepare
--- app/Day9.hs
+++ app/Day9.hs
@@ -1,49 +1,85 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+
 module Day9 where
 
 import Control.Arrow ((&&&))
-import Control.Monad.ST (ST)
-import Data.Array (Array)
-import Data.Array qualified as A
-import Data.Array.ST (STArray, runSTArray)
-import Data.Array.ST qualified as A
-import Data.Char (digitToInt)
-import Text.Parsec (Parsec, digit, getState, many1, putState, runParser)
+import Control.Monad (guard, when)
+import Control.Monad.ST (ST, runST)
+import Data.Array (assocs)
+import Data.Array.ST (STArray, modifyArray', newArray, newListArray, readArray, runSTArray, writeArray)
+import Data.Char (digitToInt, isDigit)
+import Data.Foldable (sequenceA_)
+import Data.Heap qualified as H
+import Data.List (mapAccumL, sortOn)
+import Data.Maybe (catMaybes)
 
-type Parser = Parsec String Int
+data Block = Free | File Int deriving (Show)
 
-data DiskSpot
-  = Free {freeSize :: Int}
-  | File {fileId :: Int, fileSize :: Int}
-  deriving (Show, Eq)
+data Chunk = Chunk Int Block deriving (Show)
 
-type Disk = Array Int DiskSpot
+data FileData = FileData {_id, _len, _fileNum :: Int} deriving (Show)
 
-type Input = Disk
+defragment :: [Block] -> [(Int, Block)]
+defragment blocks = assocs $ runSTArray $ do
+  let bounds = (0, length blocks - 1)
+  disk <- newListArray bounds blocks
+  let go p q
+        | p >= q = pure disk
+        | otherwise =
+            ((,) <$> readArray disk p <*> readArray disk q) >>= \case
+              (File _, _) -> go (p + 1) q
+              (_, Free) -> go p (q - 1)
+              (Free, File f) ->
+                writeArray disk p (File f)
+                  *> writeArray disk q Free
+                  *> go (p + 1) (q - 1)
+  uncurry go bounds
 
-part1 :: Input -> Input
-part1 inp = runSTArray $ do
-  mutArray <- A.thaw inp :: ST s (STArray s Int DiskSpot)
-  undefined
-
-part2 :: Input -> ()
-part2 _ = ()
-
-prepare :: String -> Input
-prepare inp = case runParser parser 0 "" inp of
-  (Left e) -> error $ show e
-  (Right res) -> res
+part1 :: String -> Int
+part1 = checksum . defragment . prepare (\len b bs -> replicate len b <> bs)
   where
-    parser :: Parser Input
-    parser = do
-      ls <- filter (/= Free 0) <$> many1 fileOrFree
-      let l = length ls
-      return $ A.listArray (0, l - 1) ls
-    fileOrFree :: Parser DiskSpot
-    fileOrFree = do
-      ind <- getState
-      f <- if even ind then File (ind `div` 2) . digitToInt <$> digit else Free . digitToInt <$> digit
-      putState (ind + 1)
-      return f
+    checksum = sum . map score
+    score (_, Free) = 0
+    score (pos, (File num)) = pos * num
+
+indexBlocks :: [Chunk] -> [(Int, Chunk)]
+indexBlocks = snd . mapAccumL go 0
+  where
+    go ix c@(Chunk len _) = (ix + len, (ix, c))
+
+part2 :: String -> Int
+part2 input = runST $ do
+  let chunks = prepare (\len b bs -> Chunk len b : bs) input
+      blocks = indexBlocks chunks
+      inputFiles = reverse [FileData ix len num | (ix, Chunk len (File num)) <- blocks]
+  freeHeaps <- newArray (1, 9) H.empty :: ST s (STArray s Int (H.Heap Int))
+  sequenceA_ $ do
+    (ix, Chunk len Free) <- blocks
+    guard $ len >= 1
+    pure $ modifyArray' freeHeaps len (H.insert ix)
+  let firstFreeBlock :: Int -> ST _ (Maybe (Int, (Int, (H.Heap Int))))
+      firstFreeBlock size = fmap (size,) . H.viewMin <$> readArray freeHeaps size
+      moveLeft orig@(FileData ix len num) = do
+        firsts <- catMaybes <$> traverse firstFreeBlock [len .. 9]
+        case sortOn (fst . snd) . filter ((< ix) . fst . snd) $ firsts of
+          [] -> pure orig
+          ((freeSize, (freeIx, h')) : _) -> do
+            writeArray freeHeaps freeSize h'
+            when (freeSize > len) $ modifyArray' freeHeaps (freeSize - len) (H.insert (freeIx + len))
+            pure $ FileData freeIx len num
+  sum . map score <$> traverse moveLeft inputFiles
+
+score :: FileData -> Int
+score (FileData ix len file) = file * (sum [ix .. ix + len - 1])
+
+prepare :: (Int -> Block -> [a] -> [a]) -> String -> [a]
+prepare f = file 0 . map digitToInt . filter isDigit
+  where
+    file _ [] = []
+    file n (size : rest) = f size (File n) $ empty n rest
+    empty _ [] = []
+    empty n (size : rest) = f size Free $ file (n + 1) rest
 
 main :: IO ()
-main = readFile "inputs/day9input.txt" >>= print . (part1 &&& part2) . prepare
+main = readFile "inputs/day9input.txt" >>= print . (part1 &&& part2)
