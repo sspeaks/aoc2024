2057c2481baeef32bac18a3a692ddc99a269fa92
sspeaks610 <marialith191@gmail.com>
2024-12-27 14:31:04 -0800

    day 13 part1 and first attempt at part 2

---
+++ app/Day13.hs
@@ -0,0 +1,54 @@
+module Day13 where
+
+import Control.Arrow ((&&&))
+import Data.Maybe (mapMaybe)
+import Text.Regex.Applicative (many, match, optional, string)
+import Text.Regex.Applicative.Common (decimal)
+
+type Coord = (Int, Int)
+
+type Button = Coord
+
+data Machine = Machine Button Button Coord deriving (Show)
+
+det :: Int -> Int -> Int -> Int -> Int
+det a b c d = a * d - b * c
+
+win :: Machine -> Maybe Coord
+win (Machine (ax, ay) (bx, by) (gx, gy))
+  | d == 0 = Nothing
+  | otherwise = case (divMod da d, divMod db d) of
+      ((a, 0), (b, 0)) -> Just (a, b)
+      _ -> Nothing
+  where
+    d = det ax bx ay by
+    da = det gx bx gy by
+    db = det ax gx ay gy
+
+type Input = [Machine]
+
+solve :: Input -> Int
+solve = sum . map score . mapMaybe win
+  where
+    score (a, b) = 3 * a + b
+
+part1 :: Input -> Int
+part1 = solve
+
+part2 :: Input -> Int
+part2 = solve . map (shiftGoalposts 10000000000000)
+  where
+    shiftGoalposts amt (Machine a b (x, y)) = Machine a b (x + amt, y + amt)
+
+prepare :: String -> Input
+prepare = maybe (error "no parse") id . match input
+  where
+    input = many (machine <* (optional $ string "\n"))
+    machine = Machine <$> line "Button A" "+" <*> line "Button B" "+" <*> line "Prize" "="
+    line label op =
+      (,)
+        <$> (string (label <> ": X" <> op) *> decimal)
+        <*> (string (", Y" <> op) *> decimal <* string "\n")
+
+main :: IO ()
+main = readFile "inputs/day13input.txt" >>= print . (part1 &&& part2) . prepare
--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,8 @@ import Day1 qualified
 import Day10 qualified
 import Day11 qualified
 import Day12 qualified
+import Day13 qualified
+import Day17 qualified
 import Day2 qualified
 import Day3 qualified
 import Day4 qualified
@@ -12,7 +14,6 @@ import Day6 qualified
 import Day7 qualified
 import Day8 qualified
 import Day9 qualified
-import Day17 qualified
 
 main :: IO ()
 main = do
@@ -29,3 +30,6 @@ main = do
   -- putStr "Day11: " >> Day11.main
   putStr "Day12: " >> Day12.main
   putStr "Day17: " >> Day17.main
+  putStr "Day13: " >> Day13.main
+
+-- putStr "Day17: " >> Day17.main
