38b7cbbeec6cc98c26ee8b9a1bf1c4d47b5b1e36
Seth Speaks <sspeaks610@gmail.com>
2025-01-02 19:10:48 -0800

    day19 attempted brute force

--- app/Day19.hs
+++ app/Day19.hs
@@ -1,14 +1,54 @@
 module Day19 where
 
 import Control.Arrow ((&&&))
-import Text.Parsec (Parsec, runParser)
+import Control.Monad (guard)
+import Control.Parallel.Strategies (NFData, parMap, rdeepseq, rseq)
+import Data.List (isPrefixOf)
+import Data.Maybe (catMaybes, mapMaybe)
+import Data.Traversable (for)
+import Debug.Trace (trace)
+import GHC.Generics (Generic)
+import Text.Parsec (Parsec, alphaNum, anyChar, many1, runParser, sepBy, spaces, string)
+
+data Color = White | Blue | Black | Red | Green deriving (Show, Eq, Generic)
+
+instance NFData Color
+
+type TargetDesign = [Color]
+
+type TowelPattern = [Color]
 
 type Parser = Parsec String ()
 
-type Input = ()
+type Input = ([TowelPattern], [TargetDesign])
 
-part1 :: Input -> ()
-part1 _ = ()
+part1 :: Input -> Int
+part1 inp = let !res = f inp in length . filter (not . null) $ res
+  where
+    f (!patts, !designs) = map (f' patts) designs
+    f' :: [TowelPattern] -> TargetDesign -> [[TowelPattern]]
+    f' [] _ = error "should never have 0 patterns"
+    f' _ [] = []
+    f' patts design =
+      let a = mapMaybe (g design) patts 
+          x =
+            mapMaybe
+              mapped
+              a
+       in map head x
+      where
+        mapped :: (TowelPattern, TargetDesign) -> Maybe [[TowelPattern]]
+        mapped (p, []) = let v = Just [[p]] in trace (show v) v
+        mapped (p, d) = case f' patts d of
+          [] -> trace ( "Nothing") Nothing
+          workings -> let v = Just $ (:) <$> [p] <*> workings in trace (show v) v
+    g :: TargetDesign -> TowelPattern -> Maybe (TowelPattern, TargetDesign)
+    g [] _ = Nothing
+    g d p
+      | p `isPrefixOf` d = let !v = Just (p, drop (length p) d) in trace (show v) v
+      | otherwise = Nothing
+
+-- length . filter (not . null) $
 
 part2 :: Input -> ()
 part2 _ = ()
@@ -19,7 +59,20 @@ prepare inp = case runParser parser () "" inp of
   (Right res) -> res
   where
     parser :: Parser Input
-    parser = return ()
+    parser = (,) <$> parseTowelPatterns <*> parseTargetDesigns
+    parseTowelPatterns :: Parser [TowelPattern]
+    parseTowelPatterns = sepBy parseColorSeq (string ", ") <* spaces
+    parseColorSeq :: Parser TowelPattern
+    parseColorSeq = many1 (getColor <$> alphaNum) <* spaces
+    parseTargetDesigns :: Parser [TargetDesign]
+    parseTargetDesigns = many1 parseColorSeq
+    getColor :: Char -> Color
+    getColor 'r' = Red
+    getColor 'w' = White
+    getColor 'u' = Blue
+    getColor 'b' = Black
+    getColor 'g' = Green
+    getColor c = error $ "Char " <> [c] <> " doesn't exist as a color"
 
 main :: IO ()
 main = readFile "inputs/day19input.txt" >>= print . (part1 &&& part2) . prepare
