4d511fc6f802be76b96b8b542351bc68f50158ea
Seth Speaks <sspeaks610@gmail.com>
2025-01-03 00:15:00 -0800

    simplified 19....

--- app/Day19.hs
+++ app/Day19.hs
@@ -1,15 +1,24 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Day19 where
 
 import Control.Arrow ((&&&))
-import Control.Monad.State qualified as ST
 import Data.List (isPrefixOf)
-import Data.Map qualified as M
+import Data.MemoTrie (HasTrie (..), Reg, enumerateGeneric, memo, trieGeneric, untrieGeneric, (:->:))
 import GHC.Generics (Generic)
 import Text.Parsec (Parsec, alphaNum, many1, runParser, sepBy, spaces, string)
 
 data Color = White | Blue | Black | Red | Green deriving (Show, Eq, Generic, Ord)
+
+instance HasTrie Color where
+  newtype Color :->: b = ColorTrie {unColorTrie :: Reg Color :->: b}
+  trie :: (Color -> b) -> Color :->: b
+  trie = trieGeneric ColorTrie
+  untrie :: (Color :->: b) -> Color -> b
+  untrie = untrieGeneric unColorTrie
+  enumerate :: (Color :->: b) -> [(Color, b)]
+  enumerate = enumerateGeneric unColorTrie
 
 type TargetDesign = [Color]
 
@@ -20,37 +29,27 @@ type Parser = Parsec String ()
 type Input = ([TowelPattern], [TargetDesign])
 
 part1 :: Input -> Int
-part1 (patts, dess) = length . filter (> 0) . map (solutionsPerDesign patts) $ dess
+part1 (patts, dess) = length . filter (> 0) . map (memoSolve patts) $ dess
 
 part2 :: Input -> Int
-part2 (patts, dess) = sum . map (solutionsPerDesign patts) $ dess
-
-solutionsPerDesign :: [TowelPattern] -> TargetDesign -> Int
-solutionsPerDesign patts design = ST.evalState (f design) M.empty
-  where
-    f :: TargetDesign -> ST.State (M.Map TargetDesign Int) Int
-    f [] = return 1
-    f des = do
-      !seen <- ST.gets (M.lookup des)
-      case seen of
-        (Just v) -> return v
-        Nothing -> do
-          let !workingPatts = filter (`isPrefixOf` des) patts
-          !viableSuffixCount <- sum <$> mapM (\p -> f $ drop (length p) des) workingPatts
-          ST.modify (M.insert des viableSuffixCount)
-          return viableSuffixCount
-
-type Solution = [TowelPattern]
+part2 (patts, dess) = sum . map (memoSolve patts) $ dess
 
 -- Brute force solution
-f' :: [TowelPattern] -> TargetDesign -> [Solution]
-f' patts des =
-  [ patt : rest
-    | patt <- patts,
-      patt `isPrefixOf` des,
-      let tailOf = drop (length patt) des,
-      rest <- f' patts tailOf
-  ]
+memoSolve :: [TowelPattern] -> TargetDesign -> Int
+memoSolve patts = memo $ \case
+  [] -> 1
+  des ->
+    let workingPatts = filter (`isPrefixOf` des) patts
+        viableSuffixCount = sum $ map (\p -> memoSolve patts $ drop (length p) des) workingPatts
+     in viableSuffixCount
+
+-- Solution to get all solutions... very space inefficient
+-- [ patt : rest
+--   | patt <- patts,
+--     patt `isPrefixOf` des,
+--     let tailOf = drop (length patt) des,
+--     n <- f' patts tailOf
+-- ]
 
 prepare :: String -> Input
 prepare inp = case runParser parser () "" inp of
