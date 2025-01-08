75ac878fd65653a4e07c73238ef6b01d9cf8f669
Seth Speaks <sspeaks610@gmail.com>
2025-01-08 12:51:23 -0800

    starting work on P2

--- app/Day15.hs
+++ app/Day15.hs
@@ -3,8 +3,10 @@
 module Day15 where
 
 import Control.Arrow ((&&&))
+import Data.Array qualified as A
 import Data.Maybe (fromJust)
-import Grid (Direction (..), Grid, dirVector, findInGrid, gridLookup, moveCell, stringToGrid, (.+.))
+import GHC.IO (unsafePerformIO)
+import Grid (Direction (..), Grid, dirVector, findInGrid, gridLookup, moveCell, printGrid, stringToGrid, (.+.))
 import Text.Parsec (Parsec, many1, manyTill, runParser, spaces, string, try)
 import Text.Parsec.Char (anyChar)
 
@@ -37,8 +39,24 @@ type Parser = Parsec String ()
 
 type Input = (Grid Cell, [Direction])
 
-canMoveDir :: Grid Cell -> (Int, Int) -> Direction -> Bool
-canMoveDir g c d = case gridLookup newC g of
+class HasCellValue a where
+  getCell :: a -> Cell
+  toSpot :: Cell -> a
+
+instance HasCellValue Cell where
+  getCell :: Cell -> Cell
+  getCell = id
+  toSpot :: Cell -> Cell
+  toSpot = id
+
+instance HasCellValue (Cell, Maybe (Int, Int)) where
+  getCell :: (Cell, Maybe (Int, Int)) -> Cell
+  getCell = fst
+  toSpot :: Cell -> (Cell, Maybe (Int, Int))
+  toSpot c = (c, Nothing)
+
+canMoveDir :: (HasCellValue a) => Grid a -> (Int, Int) -> Direction -> Bool
+canMoveDir g c d = case getCell <$> gridLookup newC g of
   Nothing -> False
   Just v
     | v == Wall -> False
@@ -47,15 +65,15 @@ canMoveDir g c d = case gridLookup newC g of
   where
     newC = c .+. dirVector d
 
-moveDir :: Grid Cell -> (Int, Int) -> Direction -> (Grid Cell, (Int, Int))
+moveDir :: (HasCellValue a) => Grid a -> (Int, Int) -> Direction -> (Grid a, (Int, Int))
 moveDir g c d
   | not $ canMoveDir g c d = (g, c)
-  | otherwise = case gridLookup newC g of
+  | otherwise = case getCell <$> gridLookup newC g of
       Nothing -> error "Shouldn't happen... we've already check the move"
       Just Box ->
         let (newG, _) = moveDir g newC d
-         in (fromJust $ moveCell newG c d (const Empty), newC)
-      _ -> (fromJust $ moveCell g c d (const Empty), newC)
+         in (fromJust $ moveCell newG c d (const (toSpot Empty)), newC)
+      _ -> (fromJust $ moveCell g c d (const (toSpot Empty)), newC)
   where
     newC = c .+. dirVector d
 
@@ -72,7 +90,21 @@ part1 (g, dirs) =
     score (x, y) = 100 * y + x
 
 part2 :: Input -> ()
-part2 _ = ()
+part2 (g, _) =
+  let newG = gridMorph g
+      v = (unsafePerformIO $ printGrid newG (cellToc . fst))
+   in v
+
+-- Each cell is a value and a Maybe indicating whether they have a neighbor or not
+gridMorph :: Grid Cell -> Grid (Cell, Maybe (Int, Int))
+gridMorph g = A.array ((0, 0), (newWidth - 1, newHeight - 1)) $ A.assocs g >>= f
+  where
+    (w, h) = snd $ A.bounds g
+    (newWidth, newHeight) = ((w + 1) * 2, h + 1)
+    f ((x, y), Box) = [((x * 2, y), (Box, Just (x * 2 + 1, y))), ((x * 2 + 1, y), (Box, Just (x * 2, y)))]
+    f ((x, y), Wall) = [((x * 2, y), (Wall, Nothing)), ((x * 2 + 1, y), (Wall, Nothing))]
+    f ((x, y), Robot) = [((x * 2, y), (Robot, Nothing)), ((x * 2 + 1, y), (Empty, Nothing))]
+    f ((x, y), Empty) = [((x * 2, y), (Empty, Nothing)), ((x * 2 + 1, y), (Empty, Nothing))]
 
 prepare :: String -> Input
 prepare inp = case runParser parser () "" inp of
